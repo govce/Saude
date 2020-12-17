c25467b4f6b73346745c6d03ccb3e6c78a5c3e90
Junior-Shyko <franciscoanto@gmail.com>
2020-12-17 10:46:20 -0300

    #14 - Adicionado as taxonomias vindo direto do banco

--- opportunity-types.php
+++ opportunity-types.php
@@ -5,6 +5,19 @@
 $app = MapasCulturais\App::i();
 $termsOp = $app->repo('Term')->findBy(['taxonomy' => 'opportunity_taxonomia']);
 $oportunidades = array_map(function($term) { return $term->term; }, $termsOp);
+
+$taxoOpportunityType = $app->repo('Term')->findBy(['taxonomy' => 'opportunity_taxonomia']);
+$typeOpportunity = [];
+$new = "";
+foreach ($taxoOpportunityType as $key => $value) {
+    //PREENCHENDO O VALOR QUE É RETORNADO DO BANCO
+    $new = $value->term;
+    //EXCLUINDO O INDICE
+    unset($chave);
+    //INSERINDO UM NOVO ARRAY NO FINAL DO ARRAY
+    array_push($typeOpportunity, ['name' => $new]);
+}
+
 return array(
     'metadata' => array(
 
@@ -84,52 +97,7 @@ return array(
             'options' => $oportunidades
         ]
     ),
-    'items' => array(
-        1 =>  array( 'name' => \MapasCulturais\i::__("Festival")),
-        2 =>  array( 'name' => \MapasCulturais\i::__("Encontro")),
-        3 =>  array( 'name' => \MapasCulturais\i::__("Sarau")),
-        4 =>  array( 'name' => \MapasCulturais\i::__("Reunião")),
-        5 =>  array( 'name' => \MapasCulturais\i::__("Mostra")),
-        6 =>  array( 'name' => \MapasCulturais\i::__("Convenção")),
-        7 =>  array( 'name' => \MapasCulturais\i::__("Ciclo")),
-        8 =>  array( 'name' => \MapasCulturais\i::__("Programa")),
-        9 =>  array( 'name' => \MapasCulturais\i::__("Edital")),
-        10 => array( 'name' => \MapasCulturais\i::__("Concurso")),
-        11 => array( 'name' => \MapasCulturais\i::__("Exposição")),
-        12 => array( 'name' => \MapasCulturais\i::__("Jornada")),
-        13 => array( 'name' => \MapasCulturais\i::__("Exibição")),
-        14 => array( 'name' => \MapasCulturais\i::__("Feira")),
-        15 => array( 'name' => \MapasCulturais\i::__("Intercâmbio Cultural")),
-        16 => array( 'name' => \MapasCulturais\i::__("Festa Popular")),
-        17 => array( 'name' => \MapasCulturais\i::__("Festa Religiosa")),
-        18 => array( 'name' => \MapasCulturais\i::__("Seminário")),
-        19 => array( 'name' => \MapasCulturais\i::__("Congresso")),
-        20 => array( 'name' => \MapasCulturais\i::__("Palestra")),
-        21 => array( 'name' => \MapasCulturais\i::__("Simpósio")),
-        22 => array( 'name' => \MapasCulturais\i::__("Fórum")),
-        23 => array( 'name' => \MapasCulturais\i::__("Curso")),
-        24 => array( 'name' => \MapasCulturais\i::__("Oficina")),
-        25 => array( 'name' => \MapasCulturais\i::__("Jornada")),
-        26 => array( 'name' => \MapasCulturais\i::__("Conferência Pública Setorial")),
-        27 => array( 'name' => \MapasCulturais\i::__("Conferência Pública Nacional")),
-        28 => array( 'name' => \MapasCulturais\i::__("Conferência Pública Estadual")),
-        29 => array( 'name' => \MapasCulturais\i::__("Conferência Pública Municipal")),
-//        30 => array( 'name' => \MapasCulturais\i::__("Parada e Desfile Militar")),
-//        31 => array( 'name' => \MapasCulturais\i::__("Parada e Desfile Cívico")),
-//        32 => array( 'name' => \MapasCulturais\i::__("Parada e Desfile Festivo")),
-//        33 => array( 'name' => \MapasCulturais\i::__("Parada e Desfile Político")),
-//        34 => array( 'name' => \MapasCulturais\i::__("Parada e Desfile de Ações Afirmativas")),
-
-        // tipos não existentes em projetos
-        40 => array( 'name' => \MapasCulturais\i::__("Abaixo-assinado")),
-        41 => array( 'name' => \MapasCulturais\i::__("Campanhas")),
-        42 => array( 'name' => \MapasCulturais\i::__("Pesquisa")),
-        43 => array( 'name' => \MapasCulturais\i::__("Oportunidade de trabalho")),
-        44 => array( 'name' => \MapasCulturais\i::__("Outros eventos")),
-        45 => array( 'name' => \MapasCulturais\i::__("Outros tipos de inscrição")),
-
-
-    )
+    'items' => $typeOpportunity
     /* EXEMPLOS DE METADADOS:
 
     'cnpj' => array(
